2f3784bc5c77791471d8839f136c7a20c29984cd
henninghk <64491564+henninghk@users.noreply.github.com>
2021-04-22 14:23:07 +0200

    Initial commit

---
+++ TilfeldigFirkanter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TilfeldigeFirkanter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var row1 = new VirtualScreenRow(20);
+            //var row2 = new VirtualScreenRow(20);
+            //var row3 = new VirtualScreenRow(20);
+            //row2.AddBoxMiddleRow(4, 5);
+            row1.AddBoxTopRow(5, 3);
+            //row3.AddBoxBottomRow(3, 7);
+            row1.Show();
+            //row2.Show();
+            //row3.Show();
+        }
+    }
+}
---
+++ TilfeldigFirkanter/VirtualScreenRow.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace TilfeldigeFirkanter
+{
+    public class VirtualScreenRow
+    {
+        private VirtualScreenCell[] _cells;
+
+        public VirtualScreenRow(int screenWidth)
+        {
+            _cells = new VirtualScreenCell[screenWidth];
+            for (int i = 0; i < screenWidth; i++)
+            {
+                var cell = new VirtualScreenCell();
+                _cells[i] = cell;
+            }
+        }
+
+        public void AddBoxTopRow(int boxX, int boxWidth)
+        {
+            for (int i = boxX; i < _cells.Length; i++)
+            {
+                if (i == boxX)
+                {
+                    _cells[i].AddUpperLeftCorner();
+                }
+                if (i == _cells.Length)
+                {
+                    _cells[i].AddUpperRightCorner();
+                }
+                else
+                {
+                    _cells[i].AddHorizontal();
+                }
+            }
+        }
+        public void AddBoxBottomRow(int boxX, int boxWidth) { }
+        public void AddBoxMiddleRow(int boxX, int boxWidth) { }
+
+        public void Show()
+        {
+            foreach (var cell in _cells)
+            {
+                Console.Write(cell.GetCharacter());
+            }
+        }
+    }
+}
